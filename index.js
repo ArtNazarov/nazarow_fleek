13fd8695a53e1420f720c8c86a46dc320a3c85dc
Artem Nazarov <programmist.nazarov@gmail.com>
2023-08-05 12:31:17 +0500

    change updater

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-use_host = 'nazarow.ru';
-
-if (window.location.hostname != "nazarow.ru") {
-  use_host = 'fleek.ipfs.io/ipfs/QmYeNpu3m3LAkJdSXJMWta8zt4nmBzq4AQYU6rP46qx88x';
-}
-
-const host = use_host; // заменить на нужный хост
+const host = "https://fleek.ipfs.io/ipfs/QmYeNpu3m3LAkJdSXJMWta8zt4nmBzq4AQYU6rP46qx88x"; // заменить на нужный хост
 const navLinks = document.querySelectorAll(".nav-menu"); // выбираем все элементы с классом nav-menu
 navLinks.forEach(link => {
   const oldHref = link.getAttribute("href"); // получаем старое значение href
-  link.setAttribute("href", 'https://' + host + oldHref); // устанавливаем новое значение href
-});
-
-
+  link.setAttribute("href", host + oldHref); // устанавливаем новое значение href
+});console.log("test");
